9f5c20eafc71e4150863a6207512702f4b218f2e
gurdeep kaur <gurdeepkaur@gurdeeps-MacBook-Pro.local>
2018-04-18 14:23:47 +1000

    create link in rails file

---
+++ config/initializers/cors.rb
@@ -0,0 +1,10 @@
+#this code was all jacked from rackcors github docs
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*',
+      :headers => :any,
+      :methods => %i(get post put patch delete options head)
+  end
+end
